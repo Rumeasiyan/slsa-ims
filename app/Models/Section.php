4c35fcefec4b2da40d6da8199558dff9131f3ece
Your Name <your-email@example.com>
2025-12-24 21:24:36 +0530

    Add sections table, model and seeder

---
+++ app/Models/Section.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Section extends Model
+{
+    protected $fillable = [
+        'name',
+        'age',
+    ];
+
+    public function groups(): HasMany
+    {
+        return $this->hasMany(Group::class);
+    }
+}
